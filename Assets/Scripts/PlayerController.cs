c26fc37f1685b63bd723fe7add57e98baa4384bb
Motherl0de <timmikvo@gmail.com>
2024-05-19 21:22:13 +0300

    Create platform for level

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,13 +8,13 @@ namespace TT
     public sealed class PlayerController : MonoBehaviour
     {
         private Inputs _inputAction;
-        private float _speed = 8f;
+        private float _speed = 0.3f;
         private Vector2 _moveVector;
         private Rigidbody _rigidbody;
         private Quaternion _currentRotation;
         private Vector3 _oldDirection = new Vector3();
-        [SerializeField] private float _walkSpeed = 10f;
-        [SerializeField] private float _runSpeed = 17f;
+        [SerializeField] private float _walkSpeed = 2f;
+        [SerializeField] private float _runSpeed = 3f;
 
         private Rigidbody RB => _rigidbody ??= GetComponentInChildren<Rigidbody>();
 
@@ -44,12 +44,12 @@ namespace TT
 
         private void FixedUpdate()
         {
-            _moveVector = _inputAction.Move.WASD.ReadValue<Vector2>() * _speed;
+            _moveVector = _inputAction.Move.WASD.ReadValue<Vector2>();
             var direction = new Vector3(_moveVector.x, 0f, _moveVector.y);
             _oldDirection = direction;
-            RB.AddForce(_oldDirection);
-            _currentRotation = Quaternion.Lerp(_currentRotation, Quaternion.LookRotation(_oldDirection), Time.fixedDeltaTime * _speed);
-            if (_oldDirection.z >= 0) RB.MoveRotation(_currentRotation);
+            transform.Translate(_oldDirection * _speed);
+            _currentRotation = Quaternion.LookRotation(_oldDirection, Vector3.up);
+            //_currentRotation = Quaternion.Lerp(_currentRotation, Quaternion.LookRotation(_oldDirection), Time.fixedDeltaTime * _speed);
         }
 
         private void OnJump(InputAction.CallbackContext context)
