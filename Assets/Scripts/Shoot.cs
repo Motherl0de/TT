11453f85985ed0548c61868eb488fb1abab37ce8
Motherl0de <timmikvo@gmail.com>
2024-05-20 14:29:38 +0300

    Add Shoot with raycast for weapon

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -1,39 +1,34 @@
-using System;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace TT
 {
     public sealed class Shoot : MonoBehaviour
     {
         private bool _isActive;
-        [SerializeField] private Transform _pointer;
-        [SerializeField] private GameObject _bulletPref;
+        private PointEffect _pointer;
+        private PointEffect Pointer => _pointer ??= GetComponentInChildren<PointEffect>();
 
-        void Update()
+        void FixedUpdate()
         {
-            AimMouse();
-            if (UnityEngine.Input.GetMouseButtonDown(0))
+            if (UnityEngine.Input.GetMouseButton(0))
             {
-                GameObject bullet = Instantiate(_bulletPref, transform.position, transform.rotation);
-                bullet.GetComponent<Rigidbody>().velocity = transform.right * 20f;
+                AimMouse();
             }
         }
 
         private void AimMouse()
         {
-            Ray ray = new Ray(transform.position, transform.forward);//Camera.main.ScreenPointToRay(UnityEngine.Input.mousePosition);
-            Debug.DrawRay(_pointer.transform.position, transform.right * 100f, Color.green);
+            Ray ray = new Ray(transform.position, transform.right);//Camera.main.ScreenPointToRay(UnityEngine.Input.mousePosition);
+            Debug.DrawRay(Pointer.transform.position, transform.right * 100f, Color.green);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
             {
-                _pointer.position = hit.point;
-            }
-
-            HpEnemy enemy = hit.collider.gameObject.GetComponent<HpEnemy>();
-            if (enemy)
-            {
-                enemy.Damage(25f);
+                Pointer.transform.position = hit.point;
+                HpEnemy enemy = hit.collider.gameObject.GetComponentInChildren<HpEnemy>();
+                if (enemy)
+                {
+                    enemy.Damage(25f);
+                }
             }
         }
     }
