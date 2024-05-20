b31be630e12f617204bc60f99ea3a14fa4b2ad62
Motherl0de <timmikvo@gmail.com>
2024-05-20 17:56:11 +0300

    wip: PlayerPref

--- Assets/Scripts/HpEnemy.cs
+++ Assets/Scripts/HpEnemy.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,9 +9,20 @@ namespace TT
         private float _hpEnemy = 100f;
         [SerializeField] private Slider _sliderHp;
 
+        private void Start()
+        {
+            if (PlayerPrefs.GetFloat("_hpEnemy") <= 0)
+            {
+                _hpEnemy = PlayerPrefs.GetFloat("_hpEnemy");
+            }
+            else { _hpEnemy = 100f; }
+            PlayerPrefs.Save();
+        }
+
         public void Damage(float damage)
         {
             _hpEnemy -= damage;
+            PlayerPrefs.SetFloat("_hpEnemy",_hpEnemy - damage);
             if (_hpEnemy <= 0)
             {
                 _sliderHp.value = 0f;
